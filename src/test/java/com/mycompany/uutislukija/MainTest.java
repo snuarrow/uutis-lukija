d15326cb8ddbadf7d4a597e9e9e6e7f192af8aa8
snuarrow <henri.vaara@helsinki.fi>
2015-09-28 16:00:07 +0300

    coveralls configuration

---
+++ src/test/java/com/mycompany/uutislukija/MainTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.uutislukija;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hexvaara
+ */
+public class MainTest
+{
+    Main main;
+    
+    public MainTest()
+    {
+        main = new Main();
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    
+    @Test
+    public void testKomennot()
+    {
+        String tmp = main.komennot();
+        assertEquals(1,1);
+    }
+    
+}
